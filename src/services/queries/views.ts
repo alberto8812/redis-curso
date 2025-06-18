69455d6c2ece501c97147105d83d941d42ad66c6
Carlos8992 <cvelasco@saludelectronica.com>
2025-06-17 22:22:45 -0500

    feat(): implmentar  script lua redis

--- src/services/queries/views.ts
+++ src/services/queries/views.ts
@@ -2,18 +2,16 @@ import { itemByViewsKeys, itemsKeys, itemsViewKey } from "$services/keys";
 import { client } from "$services/redis";
 
 export const incrementView = async (itemId: string, userId: string) => {
-
-    /**
-     * ingresa la vista para alamacenar 
-     * si ya existe retorna 0  si no retorna uno 
-    */
-    const inserted = await client.pfAdd(itemsViewKey(itemId), userId);
-    if (inserted) {
-        return Promise.all(
-            [
-                client.hIncrBy(itemsKeys(itemId), 'views', 1),
-                client.zIncrBy(itemByViewsKeys(), 1, itemId)
-            ])
-    }
+    return client.incrementView(itemId, userId)
 
 };
+
+// KEYS i need  too acces
+// 1. itemViewsKey->
+// 2. itemskey -> ITEM#45545
+// 3. itemsByvIEWSkEY
+//EVALSHA ID 3 
+
+// ARGUMENTS i NEDD TO ACCEPT
+// 1.itemId
+// 2. userId
--- src/services/redis/client.ts
+++ src/services/redis/client.ts
@@ -1,11 +1,62 @@
-import { createClient } from 'redis';
-
+import { createClient, defineScript } from 'redis';
+import { itemByViewsKeys, itemsKeys, itemsViewKey } from "$services/keys";
 const client = createClient({
 	socket: {
 		host: process.env.REDIS_HOST,
 		port: parseInt(process.env.REDIS_PORT)
 	},
-	password: process.env.REDIS_PW
+	password: process.env.REDIS_PW,
+	scripts: {
+		//DEEFINIMOS EL NOMBRE DEL SCRIPT
+		addOneAndStore: defineScript({
+			//numer de claves que queremos pasar al script
+			NUMBER_OF_KEYS: 1,
+			//ARGV es la matriz de todos los diferentes argumentos que se proporcionan
+			SCRIPT: `
+			 return redis.call('SET',KEYS[1],1 + tonumber(ARGV[1])) 
+			`,
+			transformArguments(key: string, value: number) {
+				//apartir de este retorno se retornan los dos argumentos que procesara nuestro script
+				return [key, value.toString()]
+				//['books:count','5']
+				//EVALSHA<ID> 1
+			},
+			// NO TENEMOS UN FORMA DE TRANFORMAR NI SERIALIZAR LOS DATOS
+			transformReply(reply: any) {
+				return reply
+			}
+
+		}),
+		incrementView: defineScript({
+			NUMBER_OF_KEYS: 3,
+			SCRIPT: `
+			  local itemsviewsKey=KEYS[1]
+			  local itemsKey=KEYS[2]
+			  local itemsByViewsKey=KEYS[3]	
+			  local itemId = ARGV[1]
+			  local userId =ARGV[2]		
+
+			  loca inserted = redis.call('PFADD',itemsviewsKey,userId)
+			  
+			  if inserted == 1 then 
+				redis.call('HINCRBY',itemsKey,'views',1)
+				redis.call('ZINCRBY',itemsviewsKey,1,itemId)
+			  end
+			`,
+			transformArguments(itemsId: string, userId: string) {
+				return [
+					itemsViewKey(itemsId),
+					itemsKeys(itemsId),
+					itemByViewsKeys(),
+					itemsId,
+					userId
+				]
+				//EVALSHA id 3
+			}
+			,
+			transformReply() { },
+		})
+	}
 });
 
 client.on('error', (err) => console.error(err));
